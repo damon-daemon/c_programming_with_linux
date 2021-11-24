614457d74d56eeab9745d38499e3ed00bcd420df
damon-daemon <damonshackleton@protonmail.com>
2021-11-23 21:20:04 -0500

    add structure of c program assignment

---
+++ 01_getting_started/10_structure_of_a_simple_c_program.c
@@ -0,0 +1,17 @@
+// preprocessor directive
+#include <stdio.h>
+
+int main(void){
+
+    // variable declaration
+    int i;
+
+    //  executable statement
+    printf("I ");
+    for(i = 0; i < 3 ; i++) {
+        printf("love ");
+    }
+    printf("programming!");
+
+    return(0);
+}
